4b62e23ec4fc8196a43e9e08a9efdafc43cd7e2c
Dino R <Dino@Mac.home>
2011-12-19 19:31:22 +0100

    Book depository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookDepository::Application.config.secret_token = '8c531900dce33bf446435090670d1e16cd8d7268b5662e477ad4fe65b98f3f5b12e13927917a6d353423909a7376fed779b99b3219271b7f42540141a2a297ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BookDepository::Application.config.session_store :cookie_store, key: '_book_depository_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BookDepository::Application.config.session_store :active_record_store
