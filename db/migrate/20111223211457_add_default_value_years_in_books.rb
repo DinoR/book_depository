1fc4cab5349494e9270bfac373699f8e867c835b
Dino <dino.rosic@me.com>
2011-12-24 19:36:08 +0100

    Fix and remove migration.

--- db/migrate/20111223211457_add_default_value_years_in_books.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultValueYearsInBooks < ActiveRecord::Migration
-  def up
-    remove_column :books, :year, :integer
-    add_column :books, :year, :integer, :default => 0
-  end
-
-  def down
-  end
-end
