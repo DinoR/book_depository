1087056edd3fa6b6439d974dd75bfdc132f85b1f
Dino <dino.rosic@me.com>
2012-01-05 22:05:23 +0100

    Avrage genres per author fix.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
     @authors  = Author.all
-    @avrage  = Author.all.count / Genre.where("genre != ?","N/A").count.to_f
+    #@avrage  = Author.all.count / Genre.where("genre != ?","N/A").count.to_f
+    @avrage = 0.0
+    @authors.each {|author| @avrage += author.genres.where("genre != ?", "N/A").count }
+    @avrage = @avrage.to_f/@authors.count.to_f
   end
   
   def genre
